0a4b92716278805d4bfffeb20f8b33f7d05d2a14
azhilus <tomar3007shivansh@gmail.com>
2024-01-12 20:24:49 +0530

    my_drawer.dart created

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import '../theme/theme_provider.dart';
+import 'package:habittracker/components/my_drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,17 +12,15 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        drawer: Drawer(
-          backgroundColor: Theme.of(context).colorScheme.background,
-          child: Center(
-            child: CupertinoSwitch(
-              value: Provider.of<ThemeProvider>(context).isDarkMode,
-              onChanged: (value) =>
-                  Provider.of<ThemeProvider>(context, listen: false)
-                      .toggleTheme(),
-            ),
-          ),
-        ));
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(),
+      drawer: const MyDrawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        elevation: 0,
+        backgroundColor: Theme.of(context).colorScheme.tertiary,
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
